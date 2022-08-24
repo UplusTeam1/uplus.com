1af8a6755b9cd89af2c312b6e887e961a36ac9c6
Seo dongwan <mssdfg0708@naver.com>
2022-08-24 15:57:39 +0900

    remove: Discount entity

--- backend/project/src/main/java/com/lguplus/project/discount/repository/DiscountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lguplus.project.discount.repository;
-
-import com.lguplus.project.discount.domain.Discount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DiscountRepository extends JpaRepository<Discount, Long> {
-}
