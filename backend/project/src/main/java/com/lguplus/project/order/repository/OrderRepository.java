543fbd0a5e6ebcb0bf973c2c5104743b2ad5577b
Jeon siyeong <nnaoww@naver.com>
2022-08-25 13:22:24 +0900

    feat: Order Repository 생성

---
+++ backend/project/src/main/java/com/lguplus/project/order/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.lguplus.project.order.repository;
+
+import com.lguplus.project.order.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
