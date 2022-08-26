a1b72ed5af2b53e09865df3b51c4be458c3cb600
dongwan <83165822+mssdfg0708@users.noreply.github.com>
2022-08-26 16:51:42 +0900

    feat: Swagger Config (#39)

--- backend/project/src/main/java/com/lguplus/project/device/controller/DeviceController.java
+++ backend/project/src/main/java/com/lguplus/project/device/controller/DeviceController.java
@@ -1,6 +1,9 @@
 package com.lguplus.project.device.controller;
 
 import com.lguplus.project.device.service.DeviceServiceByDongWan;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,16 +19,25 @@ public class DeviceController {
     @Autowired
     DeviceServiceByDongWan deviceServiceByDongWan;
 
+    @Operation(summary = "Device List Test")
     @GetMapping
     public ResponseEntity<?> getAllDevices() {
         return new ResponseEntity<>("Device List", HttpStatus.OK);
     }
 
+    @Operation(summary = "Get Single Device Options By DeviceCode")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK!")
+    })
     @GetMapping("/{code}")
     public ResponseEntity<?> getDeviceOptions(@PathVariable String code) {
         return new ResponseEntity<>(deviceServiceByDongWan.getDeviceOptions(code), HttpStatus.OK);
     }
 
+    @Operation(summary = "Get Single Device Prices By DeviceCode And Plan Name")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK!")
+    })
     @GetMapping("price/{code}/{planName}")
     public ResponseEntity<?> getDevicePrices(@PathVariable String code, @PathVariable String planName) {
         return new ResponseEntity<>(deviceServiceByDongWan.getDevicePrices(code, planName), HttpStatus.OK);
