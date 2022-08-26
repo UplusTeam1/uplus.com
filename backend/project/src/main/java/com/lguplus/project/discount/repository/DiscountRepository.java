c7d67ab04583c781b4a54e7d10953ac203741c2a
sangwoo <sangwoo420@gmail.com>
2022-08-26 17:12:47 +0900

    feat: 요금제 이름으로 할인 정보 질의

--- backend/project/src/main/java/com/lguplus/project/discount/repository/DiscountRepository.java
+++ backend/project/src/main/java/com/lguplus/project/discount/repository/DiscountRepository.java
@@ -1,7 +1,16 @@
 package com.lguplus.project.discount.repository;
 
 import com.lguplus.project.discount.domain.Discount;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface DiscountRepository extends JpaRepository<Discount, Long> {
+
+    @EntityGraph(attributePaths = {"device", "plan"})
+    List<Discount> findByPlan_Name(String planName);
+
 }
