9c4f73610507ebd206f02da818ed5ab2bbe413c0
Jeon siyeong <nnaoww@naver.com>
2022-08-26 15:14:13 +0900

    fix: 어노테이션 추가 @Getter, @NoArgsConstructor

--- backend/project/src/main/java/com/lguplus/project/order/domain/payload/OrderRequest.java
+++ backend/project/src/main/java/com/lguplus/project/order/domain/payload/OrderRequest.java
@@ -2,7 +2,11 @@ package com.lguplus.project.order.domain.payload;
 
 import com.lguplus.project.order.domain.DiscountType;
 import com.lguplus.project.order.domain.JoinType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
 public class OrderRequest {
 
     private String deviceCode;
