296c3d5b6ac9c19414aff3811c64db514c060765
권나영 <48379869+i-zro@users.noreply.github.com>
2022-08-27 16:22:57 +0900

    Update deploy.sh
    script 종료, 실행  수정

--- backend/project/scripts/deploy.sh
+++ backend/project/scripts/deploy.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/app/back
-
-echo "> Build 파일 복사"
-
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
-
 CURRENT_PID=$(pgrep -fla java | grep project-0.0.1-SNAPSHOT | awk '{print $1}')
 
-echo "> 구동중 어플리케이션 PID: $CURRENT_PID"   
+echo "> 실행중 어플리케이션 PID: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 실행중 애플리케이션이 없음"
+else
+    echo "> 애플리케이션 종료 PID: $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+JAR_NAME=$(ls -tr *.jar | tail -n 1 | tail -n 1)
+
+chmod +x $JAR_NAME
+
+echo "> 애플리케이션 실행: $JAR_NAME"
+
+nohup java -jar \
+    $JAR_NAME > nohup.out 2>&1 &
