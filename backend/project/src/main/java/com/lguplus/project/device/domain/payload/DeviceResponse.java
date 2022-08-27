1701da1d3019d844516478d42f497c4fb99cb96c
sangwoo420 <sangwoo420@gmail.com>
2022-08-27 18:04:24 +0900

    refactor: 할인금 계산 로직 서비스로 이동

--- backend/project/src/main/java/com/lguplus/project/device/domain/payload/DeviceResponse.java
+++ backend/project/src/main/java/com/lguplus/project/device/domain/payload/DeviceResponse.java
@@ -33,7 +33,7 @@ public class DeviceResponse {
 
     private Integer weeklySale;
 
-    public static DeviceResponse of(Device device, Discount discount) {
+    public static DeviceResponse of(Device device, List monthlyChargeList) {
         return DeviceResponse.builder()
                 .code(device.getCode())
                 .detailPerColor(device.getDeviceDetails()
@@ -42,58 +42,11 @@ public class DeviceResponse {
                         .collect(Collectors.toList()))
                 .name(device.getName())
                 .price(device.getPrice())
-                .monthlyChargeList(calculate(device.getPrice(), discount))
+                .monthlyChargeList(monthlyChargeList)
                 .recommendedDiscountIndex(0)
                 .storage(device.getStorage())
                 .weeklySale(device.getWeeklySale())
                 .build();
     }
 
-    public static List<Map<String, Object>> calculate(Integer devicePrice, Discount discount) {
-        List list = new ArrayList();
-        Integer planCharge = discount.getPlan().getPrice();
-        // 공시지원금
-
-        Map<String, Object> map = new HashMap<>();
-        List<Integer> devices = new ArrayList<>();
-        for (int i = 12; i <= 36; i += 12) {
-            devices.add((devicePrice - discount.getDeviceDiscount()) / i);
-        }
-        List<Integer> totalCharges = new ArrayList<>();
-
-        for (int i = 0; i < 3; i++) {
-            totalCharges.add(devices.get(i) + planCharge);
-        }
-        map.put("deviceCharge", devices);
-        map.put("planCharge", planCharge);
-        map.put("totalCharge", totalCharges);
-
-        list.add(map);
-
-        // 12개월
-
-        planCharge = (int) (planCharge * 0.75);
-
-        map = new HashMap<>();
-        devices = new ArrayList<>();
-        for (int i = 12; i <= 36; i += 12) {
-            devices.add(devicePrice / i);
-        }
-        totalCharges = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            totalCharges.add((int) (devices.get(i) + planCharge));
-        }
-        map.put("deviceCharge", devices);
-        map.put("planCharge", planCharge);
-        map.put("totalCharge", totalCharges);
-
-        list.add(map);
-
-        // 24개월 -> 12개월과 똑같음
-
-        list.add(map);
-
-        return list;
-
-    }
 }
