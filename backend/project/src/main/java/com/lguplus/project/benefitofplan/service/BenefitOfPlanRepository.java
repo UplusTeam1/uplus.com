9c2fb2368a5c84971ddc4f0f70b5364c12be0652
Seo dongwan <mssdfg0708@naver.com>
2022-08-29 18:14:25 +0900

    remove: Benefit Entity And BenefitOfPlan Entity

--- backend/project/src/main/java/com/lguplus/project/benefitofplan/service/BenefitOfPlanRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lguplus.project.benefitofplan.service;
-
-import com.lguplus.project.benefitofplan.domain.BenefitOfPlan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BenefitOfPlanRepository extends JpaRepository<BenefitOfPlan, Long> {
-}
