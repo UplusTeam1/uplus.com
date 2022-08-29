5a1d3eb7cfe4dae64bd79f7cda380c39babde465
Jeon siyeong <nnaoww@naver.com>
2022-08-29 11:11:32 +0900

    feat: 주문 기능 swagger 추가

--- backend/project/src/main/java/com/lguplus/project/order/controller/OrderContoller.java
+++ backend/project/src/main/java/com/lguplus/project/order/controller/OrderContoller.java
@@ -2,6 +2,9 @@ package com.lguplus.project.order.controller;
 
 import com.lguplus.project.order.domain.payload.OrderRequest;
 import com.lguplus.project.order.service.OrderService;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,16 +16,29 @@ import org.springframework.web.bind.annotation.*;
 public class OrderContoller {
 
     private final OrderService orderService;
+
+    @Operation(summary = "Order List")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK!")
+    })
     @GetMapping
     public ResponseEntity<?> getOrders(){
         return new ResponseEntity<>(orderService.getOrderList(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Create Order")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK!")
+    })
     @PostMapping
     public ResponseEntity<?> createOrder(@RequestBody OrderRequest orderRequest) {
         return new ResponseEntity<>(orderService.createOrder(orderRequest), HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete Order")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK!")
+    })
     @DeleteMapping("/{orderNumber}")
     public ResponseEntity<?> deleteOrder(@PathVariable Long orderNumber){
         orderService.deleteOrder(orderNumber);
