7d7f397f737d90db7ad080bcac8319329acab9bf
LJH <leejaehee0728@gmail.com>
2022-08-29 14:21:28 +0900

    feat: PlanNotFoundException 추가

--- backend/project/src/main/java/com/lguplus/project/plan/repository/PlanRepository.java
+++ backend/project/src/main/java/com/lguplus/project/plan/repository/PlanRepository.java
@@ -4,8 +4,10 @@ import com.lguplus.project.plan.domain.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PlanRepository extends JpaRepository<Plan, Long> {
 
-    Plan findByName(String planName);
+    Optional<Plan> findByName(String planName);
 }
