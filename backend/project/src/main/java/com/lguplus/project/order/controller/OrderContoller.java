a9ee40ea529998bfcb6cb7142c07d7bcdd4a4c52
Jeon siyeong <nnaoww@naver.com>
2022-08-29 09:39:47 +0900

    feat: 주문 기능 생성 주문 조회, 생성, 삭제

--- backend/project/src/main/java/com/lguplus/project/order/controller/OrderContoller.java
+++ backend/project/src/main/java/com/lguplus/project/order/controller/OrderContoller.java
@@ -1,25 +1,31 @@
 package com.lguplus.project.order.controller;
 
+import com.lguplus.project.order.domain.payload.OrderRequest;
+import com.lguplus.project.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("api/order")
 public class OrderContoller {
 
+    private final OrderService orderService;
     @GetMapping
     public ResponseEntity<?> getOrders(){
-        return new ResponseEntity<>("orderList", HttpStatus.OK);
+        return new ResponseEntity<>(orderService.getOrderList(), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<?> createOrder(){
-        return new ResponseEntity<>("주문", HttpStatus.OK);
+    public ResponseEntity<?> createOrder(@RequestBody OrderRequest orderRequest) {
+        return new ResponseEntity<>(orderService.createOrder(orderRequest), HttpStatus.OK);
     }
 
     @DeleteMapping("/{orderNumber}")
-    public ResponseEntity<?> deleteOrder(){
+    public ResponseEntity<?> deleteOrder(@PathVariable Long orderNumber){
+        orderService.deleteOrder(orderNumber);
         return new ResponseEntity<>("주문 삭제", HttpStatus.OK);
     }
 }
---
+++ backend/project/src/main/java/com/lguplus/project/order/exception/OrderNotFoundException.java
@@ -0,0 +1,4 @@
+package com.lguplus.project.order.exception;
+
+public class OrderNotFoundException extends RuntimeException{
+}
