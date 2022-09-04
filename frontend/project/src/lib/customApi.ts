7fd2f93ecda97b7df1191507249043eac40dbb95
LJH <leejaehee0728@gmail.com>
2022-09-05 03:57:36 +0900

    chore: msa api 수정

--- frontend/project/src/lib/customApi.ts
+++ frontend/project/src/lib/customApi.ts
@@ -1,20 +1,19 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://my-lguplus.shop:8080'
 const ITEM_BASE_URL = 'item.msa-myuplus.in:8080'
 const ORDER_BASE_URL = 'order.msa-myuplus.in:8081'
 const SEARCH_BASE_URL = 'search.msa-myuplus.in:8082'
 
 const itemApi = axios.create({
-  baseURL: BASE_URL,
+  baseURL: ITEM_BASE_URL,
 })
 
 export const orderApi = axios.create({
-  baseURL: BASE_URL,
+  baseURL: ORDER_BASE_URL,
 })
 
 export const searchApi = axios.create({
-  baseURL: BASE_URL,
+  baseURL: SEARCH_BASE_URL,
 })
 
 export default itemApi
