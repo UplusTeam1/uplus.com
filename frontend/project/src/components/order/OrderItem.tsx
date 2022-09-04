c0cf0c224386aec7285cfd782e6d34ac65bd200f
sangwoo <sangwoo420@gmail.com>
2022-09-04 12:58:52 +0900

    feat: 주문 취소 확인 로직 추가

--- frontend/project/src/components/order/OrderItem.tsx
+++ frontend/project/src/components/order/OrderItem.tsx
@@ -2,6 +2,7 @@ import { UseMutateFunction } from 'react-query'
 import styled, { useTheme } from 'styled-components'
 import { OrderData } from '../../api/order'
 import UplusButton from '../UplusButton'
+import Swal from 'sweetalert2'
 
 const OrderItemContainer = styled.div`
   display: flex;
@@ -47,7 +48,29 @@ interface OrderItemProps {
 
 function OrderItem({ order, clickDeleteButton }: OrderItemProps) {
   const theme = useTheme()
-  const price = 1000000
+  const clickButton = () => {
+    Swal.fire({
+      title: '주문 취소',
+      text: '주문을 취소하시겠습니까?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: '주문 취소',
+      cancelButtonText: '나기기',
+    }).then((result: any) => {
+      if (result.isConfirmed) {
+        // delete order
+        clickDeleteButton(order.orderNumber)
+        Swal.fire({
+          title: '주문 취소 완료',
+          text: '주문이 취소되었습니다',
+          confirmButtonColor: '#3085d6',
+          confirmButtonText: '확인',
+        })
+      }
+    })
+  }
   return (
     <OrderItemContainer>
       <ImageContainer>
@@ -72,7 +95,7 @@ function OrderItem({ order, clickDeleteButton }: OrderItemProps) {
           fontColor={theme.app.blackFont}
           bgColor={theme.app.lightGray}
           border={`1px solid ${theme.app.grayFont}`}
-          onClick={() => clickDeleteButton(order.orderNumber)}
+          onClick={() => clickButton()}
         />
       </ButtonContainer>
     </OrderItemContainer>
