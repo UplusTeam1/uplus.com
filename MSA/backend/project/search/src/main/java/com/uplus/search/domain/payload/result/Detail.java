3304ee3ee036dc33f819f6271accd5a5176a3cb3
sangwoo <sangwoo420@gmail.com>
2022-09-05 02:58:26 +0900

    FIX: 검색 결과 응답 대폭 수정

---
+++ MSA/backend/project/search/src/main/java/com/uplus/search/domain/payload/result/Detail.java
@@ -0,0 +1,21 @@
+package com.uplus.search.domain.payload.result;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Detail {
+    private String color;
+
+    private String rgb;
+
+    private List<String> picPaths;
+
+    private Integer stock;
+
+}
